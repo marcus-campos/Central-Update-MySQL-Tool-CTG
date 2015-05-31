d0633864995dd261dd5f13ef00da17a490675cc9
marcus210 <marcus_ultimate@hotmail.com>
2015-05-31 10:04:08 -0300

    Central de updates MySQL Tool CTG.
    Desenvolvido em 14/11/2011.

---
+++ AtualizadorMySQLToolCTG/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AtualizadorMySQLToolCTG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtb_statusAtualiza = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.rtb_statusAtualiza);
+            this.groupBox1.Location = new System.Drawing.Point(273, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 315);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(255, 315);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // rtb_statusAtualiza
+            // 
+            this.rtb_statusAtualiza.Location = new System.Drawing.Point(6, 19);
+            this.rtb_statusAtualiza.Name = "rtb_statusAtualiza";
+            this.rtb_statusAtualiza.Size = new System.Drawing.Size(313, 261);
+            this.rtb_statusAtualiza.TabIndex = 0;
+            this.rtb_statusAtualiza.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 286);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(313, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 339);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox rtb_statusAtualiza;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ AtualizadorMySQLToolCTG/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Collections.Specialized;
+
+
+namespace AtualizadorMySQLToolCTG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Thread downloadThread = null;
+        private int totalFiles = 0;
+        private int filesRead = 0;
+        private static int percentProgress;
+        private string knownToken = " NAME=\"";
+        private delegate void UpdateProgessCallback(
+            int filesRead, int totalFiles);
+        private delegate void SetTextCallback(string text);
+
+        private delegate void EnableBtnDownloadCallback();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetHTTPContent(StringBuilder webPageString)
+        {
+            // used on each read operation
+            byte[] buffer = new byte[8192];
+            string tempString = null;
+            int count = 0;
+
+            // prepare the web page we will be asking for
+            HttpWebRequest request =
+                (HttpWebRequest)WebRequest.Create("www.mysqltoolctg.freeoda.com");
+            // execute the request
+            HttpWebResponse response =
+                (HttpWebResponse)request.GetResponse();
+            // we will read data via the response stream
+            Stream responseStream = response.GetResponseStream();
+
+            do
+            {
+                // Fills the buffer with data
+                count = responseStream.Read(buffer, 0, buffer.Length);
+
+                if (count != 0)
+                {
+                    // Translates from bytes to ASCII text
+                    tempString = Encoding.ASCII.GetString(buffer, 0, count);
+
+                    // Continues building the string
+                    webPageString.Append(tempString);
+                }
+            }
+            while (count > 0);
+        }
+
+        private void ParseFileNamesFromWebPage(
+            StringCollection fileNames, string webPageContent)
+        {
+            // Get the string that ends with the first token.
+            int tokenIndex = webPageContent.IndexOf(knownToken);
+            webPageContent = webPageContent.Remove(0, tokenIndex + knownToken.Length);
+
+            // Parse the file to get all the file names from the file.
+            while (webPageContent.Length > 0 && tokenIndex > 0)
+            {
+                String fileName = webPageContent.Substring(0, webPageContent.IndexOf("\""));
+                fileNames.Add(fileName);
+                //labelStatusText.Text = "Adding file " + fileName + " to download...";
+
+                tokenIndex = webPageContent.IndexOf(knownToken);
+                webPageContent = webPageContent.Remove(0, tokenIndex + knownToken.Length);
+            }
+
+            // Update the total number of files in the HTTP file.
+            totalFiles = fileNames.Count;
+        }
+
+        private void Download()
+        {
+            try
+            {
+                StringBuilder webPageString = new StringBuilder();
+                StringCollection fileNames = new StringCollection();
+                
+                rtb_statusAtualiza.Text = ("Iniciando o download...");
+                // Get the HTTP content.
+                GetHTTPContent(webPageString);
+                string webPageContent = webPageString.ToString();
+
+                // Parse out all the file names.
+                ParseFileNamesFromWebPage(fileNames, webPageContent);
+
+                foreach (String fileName in fileNames)
+                {
+
+                    if (this.textExclude.Text.Length > 0 ||
+                        this.textOnlyInclude.Text.Length > 0)
+                    {
+                        if (fileName.Contains(this.textExclude.Text) ||
+                            !fileName.Contains(this.textOnlyInclude.Text))
+                        {
+
+                            UpdateProgessCallback delegateCB =
+                                new UpdateProgessCallback(UpdateProgress);
+                            this.Invoke(delegateCB,
+                                new object[] { filesRead++, totalFiles });
+
+
+                            continue;
+                        }
+                    }
+
+
+                    DownloadFile(
+                        textBoxRemoteUrl.Text + "/" + fileName,
+                        textLocalPath.Text + fileName);
+                }
+
+                UpdateStatusBox("Download Completed...");
+            }
+            catch (NotSupportedException exception)
+            {
+                UpdateStatusBox("Exception! " + exception.Message);
+            }
+            catch (WebException exception)
+            {
+                UpdateStatusBox("Exception! " + exception.Message);
+            }
+            catch (InvalidOperationException exception)
+            {
+                UpdateStatusBox("Exception! " + exception.Message);
+            }
+            finally
+            {
+                // Check if this method is running on a different thread
+                // than the thread that created the control.
+                if (this.textStatus.InvokeRequired)
+                {
+                    // It's on a different thread, so use Invoke.
+                    EnableBtnDownloadCallback delegateCB =
+                        new EnableBtnDownloadCallback(EnableBtnDownload);
+                    this.Invoke(delegateCB);
+                }
+                else
+                {
+                    btnStartDownload.Enabled = true;
+                }
+            }
+
+
+        }
+    }
+}
